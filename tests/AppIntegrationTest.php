4600d4888560085da46bf1f6ea9ff70fe9e632a7
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-03-12 09:01:09 +0300

    phpunit update

--- tests/AppIntegrationTest.php
+++ tests/AppIntegrationTest.php
@@ -28,20 +28,20 @@ class AppIntegrationTest extends AbstractTestCase
      */
     private $client;
 
-    public static function setUpBeforeClass()
+    public static function setUpBeforeClass(): void
     {
         static::$server1 = new Server(HTTP_MOCK_PORT, HTTP_MOCK_HOST);
         static::$server1->start();
     }
 
-    public static function tearDownAfterClass()
+    public static function tearDownAfterClass(): void
     {
         static::assertSame('', (string) static::$server1->getOutput(), (string) static::$server1->getOutput());
         static::assertSame('', (string) static::$server1->getErrorOutput(), (string) static::$server1->getErrorOutput());
         static::$server1->stop();
     }
 
-    public function setUp()
+    public function setUp(): void
     {
         static::$server1->clean();
         $this->client = static::$server1->getClient();
