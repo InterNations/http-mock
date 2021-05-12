13f2853bbdfc988d4ba469846b23220150eb232d
Lars Strojny <lars.strojny@internations.org>
2021-05-12 16:20:02 +0200

    Next

--- src/Http/MiddlewareSupportingClient.php
+++ src/Http/MiddlewareSupportingClient.php
@@ -4,28 +4,41 @@ namespace InterNations\Component\HttpMock\Http;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use SplStack;
+use function array_reduce;
 
 final class MiddlewareSupportingClient implements ClientInterface
 {
     private ClientInterface $client;
 
-    private array $middlewareStack;
+    /** @var SplStack|iterable<ClientMiddleware> */
+    private SplStack $middlewareStack;
 
     public function __construct(ClientInterface $client, ClientMiddleware ...$middlewareStack)
     {
         $this->client = $client;
-        $this->middlewareStack = array_merge([new EmptyMiddleware()], $middlewareStack);
+        $this->middlewareStack = array_reduce(
+            $middlewareStack,
+            static function (SplStack $stack, ClientMiddleware $middleware) {
+                $stack->push($middleware);
+
+                return $stack;
+            },
+            new SplStack()
+        );
     }
 
     public function sendRequest(RequestInterface $request): ResponseInterface
     {
-        $middlewareStack = $this->middlewareStack;
+        return self::createNext(clone $this->middlewareStack)($request, $this->client);
+    }
 
-        $next = static function(RequestInterface $request, ClientInterface $client) use (&$middlewareStack, &$next) {
-            $middleware = array_pop($middlewareStack);
-            return $middleware->process($request, $client, $next);
-        };
-
-        return $next($request, $this->client);
+    /** @param SplStack|iterable<ClientMiddleware> $pendingMiddlewareStack */
+    private static function createNext(SplStack $pendingMiddlewareStack): callable
+    {
+        return static fn (RequestInterface $request, ClientInterface $client) =>
+            count($pendingMiddlewareStack) > 0
+            ? $pendingMiddlewareStack->pop()->process($request, $client, self::createNext($pendingMiddlewareStack))
+            : $client->sendRequest($request);
     }
 }
